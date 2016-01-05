ae1cb71a4cdc26186c7efe045809ec50cc740c7b
Quincy Jones <qjones@c4solutions.net>
2016-01-04 20:19:32 -0600

    Added Odometry Variables.  Still need to publish

--- johnny5_controller/src/base_controller.cpp
+++ johnny5_controller/src/base_controller.cpp
@@ -14,12 +14,12 @@ BaseController::BaseController() :
   _odom_frame("odom"), // For TF
   _base_controller_rate(10.0), // 10 hz
   _base_controller_timeout(1.0), // 1 Second
-  _stopped(true),
+  _stopped(false),
   _wheel_diameter(0.1524), // 6 in
   _wheel_track(0.381),	  // 15 in
   _encoder_resolution(64), 
   _gear_reduction(29),
-  _accel_limit(0.5),
+  _accel_limit(0.1),
   _ticks_per_meter(3876.53),
   _max_accel(1.0),
   _x(0),
@@ -47,8 +47,8 @@ void BaseController::spin()
     while(ros::ok())
     {
         ros::spinOnce();
-	_current_time = ros::Time::now();
 	_last_time = _current_time;
+	_current_time = ros::Time::now();
 	update();
         rate.sleep();
     }
@@ -73,18 +73,26 @@ bool BaseController::init()
   _microcontroller.reset_encoders();
   
   // Update Publishers and Subscribers
-  _odom_pub = _nh.advertise<nav_msgs::Odometry>("/odom", 50);
+  _odom_pub = _nh.advertise<nav_msgs::Odometry>(_odom_frame, 50);
   _cmd_vel_sub = _nh.subscribe("/cmd_vel", 1, &BaseController::cmd_vel_callback, this);
 }
 void BaseController::update()
 {
   _current_time = ros::Time::now();
   
+  // Variables
+  float dxy_ave;
+  float d_th;
+  float v_xy;
+  float v_th;
+  float d_right;
+  float d_left;
+  
   // Get Encoder Values
   int left_enc;
   int right_enc;
   bool bSuccess = _microcontroller.get_encoder_ticks(left_enc, right_enc);
-  ROS_INFO("Left: %d, Right: %d", left_enc, right_enc);
+  //ROS_INFO("Left: %d, Right: %d", left_enc, right_enc);
   //try
   //catch
   // blah
@@ -96,14 +104,87 @@ void BaseController::update()
   double dt = _dt.toSec();
   
   // Compute Odometry
+  // TODO: Need Error Checking here if we didn't get encoder values
+  d_right = (right_enc - _encoder_right) / _ticks_per_meter;
+  d_left = (left_enc - _encoder_left) / _ticks_per_meter;
   
-  _microcontroller.drive(_v_target_left, _v_target_right);
+  // Cache the Value
+  _encoder_left = left_enc;
+  _encoder_right = right_enc;
+  
+  dxy_ave = (d_right + d_left) * 0.5;
+  d_th = (d_right - d_left) / _wheel_track;
+  v_xy = dxy_ave / dt;
+  v_th = d_th / dt;
+  
+  if(dxy_ave != 0)
+  {
+    float d_x = cos(d_th) * dxy_ave;
+    float d_y = -sin(d_th) * dxy_ave;
+    _x += (cos(_th) * d_x - sin(_th) * d_y);
+    _y += (sin(_th) * d_x + cos(_th) * d_y);
+  }
+  
+  if(d_th != 0)
+  {
+    _th += d_th;
+  }
+  
+  if(_current_time > _last_cmd_vel + ros::Duration(_base_controller_timeout))
+  {
+    _v_target_left = 0;
+    _v_target_right = 0;
+  }
+  
+  // Left
+  if(_v_left < _v_target_left)
+  {
+    _v_left += _max_accel;
+    if(_v_left > _v_target_left)
+    {
+      _v_left = _v_target_left;
+    }
+  }
+  else
+  {
+    // v_left = min(_v_left + _max_accel, _v_target_left);
+    _v_left -= _max_accel;
+    if(_v_left < _v_target_left)
+    {
+      _v_left = _v_target_left;
+    }
+  }
+  
+  // Right
+    if(_v_right < _v_target_right)
+  {
+    _v_right += _max_accel;
+    if(_v_right > _v_target_right)
+    {
+      _v_right = _v_target_right;
+    }
+  }
+  else
+  {
+    // _v_right = min(_v_right + _max_accel, _v_target_left);
+    _v_right -= _max_accel;
+    if(_v_right < _v_target_right)
+    {
+      _v_right = _v_target_right;
+    }
+  }
+  
+  if(!_stopped)
+  {
+    _microcontroller.drive(_v_target_left,_v_target_right);
+  }
+
 }
 void BaseController::stop()
 {
   // Set Motor Speeds to Zero/Off
-  //micro.drive(0,0);
   _stopped = true;
+  _microcontroller.drive(0,0);
   
 }
 
